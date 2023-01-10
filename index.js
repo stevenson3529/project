74e220473c91d4b07f3aa3ba3e37a2089ce8d3e6
Alexander Stevenson <alexanderfstevenson@aol.com>
2023-01-10 16:53:16 +0000

    URL parameters

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/menu.html'));
-app.listen(port, () => console.log(`app listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`))
+
+app.get('/game', (req,res) => {
+    if (req.query.mode === 'singleplayer'){
+        res.sendFile(__dirname + '/spgame.html')
+    }else if (req.query.mode === 'multiplayer'){
+        res.sendFile(__dirname + '/twgame.html')
+    }else if (req.query.mode === 'online'){
+        res.sendFile(__dirname + '/scgame.html')
+    }
+});
+
