54a022618ef1ff59da7f2e2d798d552b4c4df9e8
Alexander Stevenson <alexanderfstevenson@aol.com>
2023-01-10 16:25:27 +0000

    route to menu

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send(('menu.html')));
+app.get('/', (req, res) => res.sendFile(__dirname + '/menu.html'));
 app.listen(port, () => console.log(`app listening on port ${port}`))
