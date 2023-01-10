d27c8be95d7c4b2ab762c8e1fff077f60e58c29d
Alexander Stevenson <alexanderfstevenson@aol.com>
2023-01-10 13:18:15 +0000

    create node root

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send(('menu.html')));
+app.listen(port, () => console.log(`app listening on port ${port}`))
