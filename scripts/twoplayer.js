f16043a63223d53d0d6054d1c3554d717219f775
Alexander Stevenson <alexanderfstevenson@aol.com>
2023-01-10 18:32:38 +0000

    grid

--- scripts/twoplayer.js
+++ scripts/twoplayer.js
@@ -1,8 +1,21 @@
-const canvas = document.getElementById('#canvas');
+const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 function createBoard(){
-    context.strokeRect(0,0,450,450);
+    context.fillRect(0,0,466,466);
+    //top row
+    context.clearRect(4,4,150,150);
+    context.clearRect(158,4,150,150);
+    context.clearRect(312,4,150,150);
+    //middle row
+    context.clearRect(4,158,150,150);
+    context.clearRect(158,158,150,150);
+    context.clearRect(312,158,150,150);
+    //bottom row
+    context.clearRect(4,312,150,150);
+    context.clearRect(158,312,150,150);
+    context.clearRect(312,312,150,150);
+    
 }
 
 createBoard();
